6dda6b9cd316161c4898a9ba5d27674a93522594
Starck43 <saloon.as@gmail.com>
2024-12-13 18:31:18 +0300

    v1.0.9 add noscript to mail.ru counter

--- src/components/mailru-metrika/TopMailCounter.tsx
+++ src/components/mailru-metrika/TopMailCounter.tsx
@@ -4,24 +4,35 @@ import React from 'react'
 import Script from 'next/script'
 
 const TopMailCounter = ({ enabled }: { enabled: boolean }) => {
-    const MAILRU_ID = parseInt(process.env.NEXT_PUBLIC_MAILRU_ID || '')
+    const ID = parseInt(process.env.NEXT_PUBLIC_MAILRU_ID || '')
 
-    if (!MAILRU_ID || !enabled) return null
+    if (!ID || !enabled) return null
 
     return (
-        <Script id="top-mailru">
-            {`
-                var _tmr = window._tmr || (window._tmr = []); 
-                _tmr.push({id: "${MAILRU_ID}", type: "pageView", start: (new Date()).getTime()}); 
-                (function (d, w, id) { 
-                  if (d.getElementById(id)) return; 
-                  var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id; 
-                  ts.src = "https://top-fwz1.mail.ru/js/code.js"; 
-                  var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);}; 
-                  if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } 
-                })(document, window, "tmr-code"); 
-            `}
-        </Script>
+        <>
+            <Script id="top-mailru">
+                {`
+                    var _tmr = window._tmr || (window._tmr = []); 
+                    _tmr.push({id: "${ID}", type: "pageView", start: (new Date()).getTime()}); 
+                    (function (d, w, id) { 
+                      if (d.getElementById(id)) return; 
+                      var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id; 
+                      ts.src = "https://top-fwz1.mail.ru/js/code.js"; 
+                      var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);}; 
+                      if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } 
+                    })(document, window, "tmr-code"); 
+                `}
+            </Script>
+            <noscript>
+                <div>
+                    <img
+                        src={`https://top-fwz1.mail.ru/counter?id=${ID};js=na`}
+                        style={{ position: 'absolute', left: '-9999px' }}
+                        alt="Top.Mail.Ru"
+                    />
+                </div>
+            </noscript>
+        </>
     )
 }
 
