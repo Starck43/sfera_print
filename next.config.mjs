26dab8a4ee24014f4d4952a49fbfcfa01e13f075
Starck43 <saloon.as@gmail.com>
2025-08-20 15:36:23 +0300

    add trailing slash to config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import withBundleAnalyzer from '@next/bundle-analyzer'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: process.env.NODE_ENV === 'development' ? 'standalone' : 'export',
+    trailingSlash: true,
     reactStrictMode: true,
     productionBrowserSourceMaps: process.env.NODE_ENV === 'development',
     images: {
